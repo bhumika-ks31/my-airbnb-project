5d0f1f606fe32af55e0835de084d6eee98555f35
Bhumika Kashyap <tilantajagra@gmail.com>
2025-05-03 11:20:17 +0530

    Add favicon.ico and update HTML

--- models/Alert.js
+++
@@ -1,8 +0,0 @@
-
-const mongoose = require('mongoose');
-
-const alertSchema = new mongoose.Schema({
-  location: { type: String, required: true },
-});
-
-module.exports = mongoose.model('Alert', alertSchema);
